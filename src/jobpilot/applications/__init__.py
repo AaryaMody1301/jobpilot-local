21ade4fe75803c2ba2de2019c4b3cdadb7778673
Aarya Linesh Mody <aaryamody0418@gmail.com>
2026-09-13 12:06:50 +0530

    phase6: integrate engine with existing applications package

--- src/jobpilot/applications/__init__.py
+++ src/jobpilot/applications/__init__.py
@@ -1,1 +1,15 @@
-"""Application adapter contracts."""
+"""Application adapter and controlled-engine contracts."""
+
+from jobpilot.applications.engine import (
+    ApplicationJournal,
+    ApplicationWorker,
+    ControlledFixtureViolation,
+    question_context_sha256,
+)
+
+__all__ = [
+    "ApplicationJournal",
+    "ApplicationWorker",
+    "ControlledFixtureViolation",
+    "question_context_sha256",
+]
