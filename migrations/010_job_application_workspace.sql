680288608a8a4dcf55be39b20e79f11f41cf2e09
Aarya Linesh Mody <aaryamody0418@gmail.com>
2026-09-21 12:55:14 +0530

    Add job and application workspace metadata

---
+++ migrations/010_job_application_workspace.sql
@@ -0,0 +1,9 @@
+ALTER TABLE discovered_jobs ADD COLUMN compensation_text TEXT;
+ALTER TABLE discovered_jobs ADD COLUMN application_deadline TEXT;
+
+ALTER TABLE application_attempts ADD COLUMN follow_up_at TEXT;
+ALTER TABLE application_attempts ADD COLUMN notes TEXT NOT NULL DEFAULT '';
+ALTER TABLE application_attempts ADD COLUMN next_action TEXT NOT NULL DEFAULT '';
+
+CREATE INDEX IF NOT EXISTS idx_application_attempts_follow_up
+ON application_attempts(follow_up_at, state);
