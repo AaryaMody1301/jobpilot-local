f4de0e939c130d081b7aaaac6f898abc93501997
Aarya Linesh Mody <aaryamody0418@gmail.com>
2026-09-21 13:13:59 +0530

    Keep workspace edits separate from application journal timestamps

--- migrations/010_job_application_workspace.sql
+++ migrations/010_job_application_workspace.sql
@@ -4,6 +4,7 @@ ALTER TABLE discovered_jobs ADD COLUMN application_deadline TEXT;
 ALTER TABLE application_attempts ADD COLUMN follow_up_at TEXT;
 ALTER TABLE application_attempts ADD COLUMN notes TEXT NOT NULL DEFAULT '';
 ALTER TABLE application_attempts ADD COLUMN next_action TEXT NOT NULL DEFAULT '';
+ALTER TABLE application_attempts ADD COLUMN workspace_updated_at TEXT;
 
 CREATE INDEX IF NOT EXISTS idx_application_attempts_follow_up
 ON application_attempts(follow_up_at, state);
